7745c6cb0b792d58c9dd8a687eb07cc94b30c3f1
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2024-01-05 13:01:22 +0100

    Fix issues reported by the pre-commit (#24)

--- cleedpy/cli/rfactor.py
+++ cleedpy/cli/rfactor.py
@@ -6,10 +6,10 @@ from .. import rfactor
 
 
 @click.command("cli")
-@click.option("--input", "-i", help="Input file", required=True)
-def cli(input):
+@click.option("--config", "-c", help="Config file", required=True)
+def cli(config):
     """Command line interface for the rfactor tool"""
-    with open(input) as f:
+    with open(config) as f:
         data = yaml.safe_load(f)
 
     y_true = np.array(data["y_true"])
