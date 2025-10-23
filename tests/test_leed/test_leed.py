0551bc46f4b7b502a0937e002f6c97a9760cc9db
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2025-10-23 10:30:27 +0200

    Rename example folders to be more explicit

--- tests/test_leed/test_leed.py
+++ tests/test_leed/test_leed.py
@@ -10,8 +10,8 @@ from cleedpy.physics.constants import HART
 @pytest.mark.parametrize(
     "folder",
     [
-        "../../examples/cleedpy_example_intermediate/",
-        "../../examples/ni111_2x2O/",
+        "../../examples/ni111_cu_leed/",
+        "../../examples/ni111_2x2O_leed/",
     ],
 )
 def test_leed(folder):
